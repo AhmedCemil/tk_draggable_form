a30cde072112b3ac2f933adeda36ead7a9647545
Ahmed Cemil BİLGİN <70539255+AhmedCemil@users.noreply.github.com>
2023-05-11 08:00:45 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="draggableform",
+    version="0.1",
+    description="Draggable TKinter Form",
+    author="Ahmed Cemil Bilgin",
+    author_email="ahmed.c.bilgin@gmail.com",
+    packages="acb_draggable_form",
+)
